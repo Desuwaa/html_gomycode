35c930e2ac40f304c4d4b587ac83ec09b69a0f3e
nmasi322 <edehdivine042@gmail.com>
2023-07-09 00:56:00 +0100

    javascript issues

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
-const button = document.querySelector(".addToCart");
+const buttons = document.querySelectorAll(".addToCart");
 const done = document.querySelector(".done");
+const itemsInCart = document.getElementById("count");
+
+let count = 0;
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    count++;
+    itemsInCart.innerHTML = count;
+  });
+});
